d2214b319009fc7c536d819838543b0b6d072768
uberbrent <bmjohns1337@gmail.com>
2020-12-05 15:07:06 -0600

    updated files and completed routing

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes.js');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
